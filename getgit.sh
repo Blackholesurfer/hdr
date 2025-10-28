cfec4fd3955014c89a88f2bb7e60b3069eec2e05
Daniel Hudsky <hudsky@gmail.com>
2025-10-27 21:26:51 -0500

    Update something

--- getgit.sh
+++ getgit.sh
@@ -1,21 +1,5 @@
-# 1) Identify yourself (change the email!)
-git config --global user.name "Daniel Hudsky"
-git config --global user.email "hudsky@gmail.com"
-
-# 2) Ensure we're on 'main'
-git checkout -B main
-
-# 3) Make an initial commit (pick ONE of the two options)
-
-# Option A: You already have files to commit
-git add .
-git commit -m "Initial commit"
-
-# Option B: Repo is empty — create a README and commit
-# echo "# photon" > README.md
-# git add README.md
-# git commit -m "Initial commit"
-
-# 4) Push to GitHub
+git reset --soft HEAD~1
+echo gittoken >> .gitignore
+git rm --cached -f gittoken
+git commit -m "Initial commit (remove secret)"
 git push -u origin main
-
---
+++ updagegit.sh
@@ -0,0 +1,6 @@
+git add .
+git commit -m "Update something"
+git push
+git pull
+git clone git@github.com:Blackholesurfer/photon.git
+
